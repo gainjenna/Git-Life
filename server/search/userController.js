7ae0f6003f485a9c7f9f2811ff04679bc41f3f63
Jenna Gain <gain.jenna@gmail.com>
2016-05-09 14:55:50 -0700

    Core: console log removed

--- server/search/userController.js
+++ server/search/userController.js
@@ -33,7 +33,6 @@ module.exports = function(body, res){
         return callback(err);
       }
       var contributors = JSON.parse(contributor);
-      console.log(contributors.length)
         contributors.forEach(function(element, index){
             if (userObj[element.login]){
               userObj[element.login].count++;
