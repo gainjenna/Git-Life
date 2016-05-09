b6801174b2576ea4de56a9d6560a28331137b426
Jenna Gain <gain.jenna@gmail.com>
2016-05-09 14:55:13 -0700

    Bug: User count now is the same as repository count

--- server/search/userController.js
+++ server/search/userController.js
@@ -33,6 +33,7 @@ module.exports = function(body, res){
         return callback(err);
       }
       var contributors = JSON.parse(contributor);
+
         contributors.forEach(function(element, index){
             if (userObj[element.login]){
               userObj[element.login].count++;
