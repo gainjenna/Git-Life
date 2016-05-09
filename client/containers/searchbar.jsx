a396c176c1df6342b318b32175ae409d2df29f69
Jenna Gain <gain.jenna@gmail.com>
2016-05-09 15:48:32 -0700

    Bug: You can now submit the form by pressing enter

--- client/containers/searchbar.jsx
+++ client/containers/searchbar.jsx
@@ -9,47 +9,28 @@ export default class SearchBar extends Component {
     super(props)
     this.state = {searchTerm: ''};
   }
-
-  componentDidMount() {
-      $(document.body).on('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-      $(document.body).off('keydown', this.handleKeyDown);
-  }
-
   handleSearch(value){
     this.props.onSearchTermChange(value);
     this.setState({searchTerm: value});
     this.props.onRequest(value);
   }
-
   handleChange(value){
     this.setState({searchTerm: value});
   }
-
-  handleKeyDown(e) {
-
-    //let searchTerm = this.state.searchTerm
-    let ENTER = 13;
-    if( e.keyCode == ENTER ) {
-    }
+  onEnterPress(event){
+    event.preventDefault();
+    event.stopPropagation();
   }
-
-
   render() {
     return(
 
       <div className="input-field ">
-        <input   style={{margin:'auto', width:'50%', height:'100%'}}
-          onChange={(event) => {this.handleChange(event.target.value)}}
-          placeholder="Search GitHub" />
-        <button
-          className="btn waves-effect waves-light"
-          onClick={(event)=>{ this.handleSearch(this.state.searchTerm)}}>
-          Search
-        </button>
-        <LanguageSelect />
+        <form className="searchForm" onSubmit={this.onEnterPress}>
+          <input style={{margin:'auto', width:'50%', height:'100%'}}
+            onChange={(event) => {this.handleChange(event.target.value)}}
+            placeholder="Search GitHub" />
+          <button className="btn waves-effect waves-light" type='submit' onClick={(event)=>{ this.handleSearch(this.state.searchTerm)}}><Link to="search">Search</Link></button>
+        </form>
       </div>
 
     );
